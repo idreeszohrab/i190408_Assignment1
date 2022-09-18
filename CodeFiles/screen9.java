8414a13f3dcbd37ef50171f31ed34d324fcde9f1
idreeszohrab <idreeszohrab47@gmail.com>
2022-09-18 18:45:47 +0500

    final commit

---
+++ CodeFiles/screen9.java
@@ -0,0 +1,14 @@
+package com.johndoe.i190408;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen9);
+    }
+}
